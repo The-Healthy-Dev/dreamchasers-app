ef4cc4e80429eed681edb2beab44637e689ea8ed
tony-sn <tony.sn@outlook.com>
2023-03-16 20:33:26 +1100

    wip (feat): test latest react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import logger from './logger'
-import App from './App'
+
+import Root from './routes/root'
+
 import './index.css'
 
 window.logger = logger
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
